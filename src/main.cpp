d5058cecfb76bada16e2e22df6f6e59358283834
Daan de Graaf <daandegraaf9@gmail.com>
2024-01-02 23:46:18 +0100

    Lots of ants.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/Graphics/Transform.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <iostream>
+#include <math.h>
 
 struct Ant {
   sf::Vector2f position;
@@ -31,7 +32,7 @@ int main() {
   float antCircleDegree = 0;
   auto antBasePosition = sf::Vector2f(1000, 500);
 
-  std::vector<Ant> ants(100);
+  std::vector<Ant> ants(20);
   for (auto &ant : ants) {
     ant.stepCounter = std::rand() % 63;
     ant.position = sf::Vector2f(std::rand() % 1900, std::rand() % 1000);
@@ -61,15 +62,37 @@ int main() {
     }
 
     for (auto &ant : ants) {
-      ant.velocity += ((std::rand() % 11) - 5) / 30.f;
+      if (std::rand() % 5 == 0) {
+        ant.velocity += ((std::rand() % 11) - 5) / 20.f;
+      }
       ant.velocity = std::max(ant.velocity, 0.f);
-      ant.rotation += (std::rand() % 361 - 180) / 50.0f;
+      ant.velocity = std::min(ant.velocity, 4.0f);
+      if (std::rand() % 20 == 0) {
+        // Periodically sample a new rotation
+        ant.rotation += (std::rand() % 361 - 180) / 10.0f;
+      }
 
       sf::Transform antTransform;
 
       antTransform.rotate(ant.rotation);
       ant.position +=
           antTransform.transformPoint(sf::Vector2f(0, -ant.velocity));
+
+      if (ant.position.x < -100) {
+        ant.position.x = windowWidth + 100;
+      } else if (ant.position.x > windowWidth + 100) {
+        ant.position.x = -100;
+      }
+
+      if (ant.position.y < -100) {
+        ant.position.y = windowHeight + 100;
+      } else if (ant.position.y > windowHeight + 100) {
+        ant.position.y = -100;
+      }
+
+      // ant.position.x = fmodf(ant.position.x - 100, windowWidth + 100) + 100;
+      // ant.position.y = fmodf(ant.position.y - 100, windowHeight + 100) + 100;
+
       ant.stepCounter++;
       if (ant.stepCounter == 62) {
         ant.stepCounter = 0;
